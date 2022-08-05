9741bb120dab19e11678d4786e67c3d8ac9b1cdf
Michael Sanchez <96930354+MikeSanDev@users.noreply.github.com>
2022-08-05 01:38:38 -0500

    Checker Board
    A customizable checkerboard that you can change w x h size and colors

--- server.py
+++ server.py
@@ -2,23 +2,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/play')
-def blueboxes():
-    return render_template("play.html")
+@app.route('/')
+def index():
+    return render_template('check.html', row=8, col=8, color_one='red', color_two='black')
 
 
-@app.route("/play/<number_of_boxes>")
-def block_repeat(number_of_boxes):
-    repeat = int(number_of_boxes)
-    return render_template('play2.html', repeat=repeat)
+@app.route('/<int:x>')
+def row(x):
+    return render_template('check.html', row=x, col=8, color_one='red', color_two='black')
 
 
-@app.route("/play/<number_of_boxes>/<color_change>")
-def box_color(number_of_boxes, color_change):
-    repeat = (int(number_of_boxes))
-    colorChange = color_change
-    return render_template('play3.html', repeat=repeat, colorChange=colorChange)
+@app.route('/<int:x>/<int:y>')
+def row_col(x, y):
+    return render_template('check.html', row=x, col=y, color_one='red', color_two='black')
 
 
-if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
-    app.run(debug=True)    # Run the app in debug mode.
+@app.route('/<int:x>/<int:y>/<string:one>')
+def row_col_one(x, y, one):
+    return render_template('check.html', row=x, col=y, color_one=one, color_two='black')
+
+
+@app.route('/<int:x>/<int:y>/<string:one>/<string:two>')
+def row_col_two(x, y, one, two):
+    return render_template('check.html', row=x, col=y, color_one=one, color_two=two)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
