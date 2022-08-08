43c9d298e3dac125dfb39175883da3a0290b9f71
Michael Sanchez <96930354+MikeSanDev@users.noreply.github.com>
2022-08-08 14:47:59 -0500

    Dojo Survey
    learned how to create a survey that redirects to a results page displaying input values from a client

--- server.py
+++ server.py
@@ -1,29 +1,33 @@
 from flask import Flask, render_template, session, request, redirect
-import random
-
 app = Flask(__name__)
-
-app.secret_key = "number sauce"
+app.secret_key = "survey sauce"
 
 
 @app.route('/')
 def index():
-    if "num" not in session:
-        session["num"] = random.randint(1, 100)
-
     return render_template('index.html')
 
 
-@app.route('/guess', methods=['POST'])
-def guess():
-    session['guess'] = int(request.form['guess'])
-    return redirect('/')
+@app.route('/process_form', methods=['POST'])
+def redirect_submit():
+    session['name'] = request.form['name']
+    session['locations'] = request.form['locations']
+    session['favorites'] = request.form['favorites']
+    session['comments'] = request.form['comments']
+    print(request.form)
+    print(session)
+    return redirect('/result')
 
 
-@app.route('/reset')
-def reset():
-    session.clear()
-    return redirect('/')
+@app.route('/result')
+def result():
+    result_form = {
+        'name': session['name'],
+        'locations': session['locations'],
+        'favorites': session['favorites'],
+        'comments': session['comments'],
+    }
+    return render_template('result.html', result_form=result_form)
 
 
 if __name__ == "__main__":
